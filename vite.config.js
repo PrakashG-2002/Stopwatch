6fa2ad1403290c88e1d02d5c71c5195a149173b7
PrakashG-2002 <prakash1592002@gmail.com>
2024-07-09 14:17:39 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Stopwatch/', // This is the base URL of your GitHub Pages site
+
+})
